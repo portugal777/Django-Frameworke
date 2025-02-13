55508d001621bd7b1364cb22136a9fac8408ec76
portugal777 <sanjaarova314@gmail.com>
2025-02-13 21:14:27 +0500

    Blog app

--- DFA/settings.py
+++ DFA/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
@@ -47,7 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'Blog.apps.BlogConfig',
+
 ]
 
 ROOT_URLCONF = 'DFA.urls'
